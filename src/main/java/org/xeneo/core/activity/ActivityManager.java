e54825bb6af8c1fbe0bac06b899d07763759c951
Stefan Huber <stefan.huber.mail@gmail.com>
2012-05-05 16:31:43 +0200

    site configuration

--- src/main/java/org/xeneo/core/activity/ActivityManager.java
+++ src/main/java/org/xeneo/core/activity/ActivityManager.java
@@ -3,27 +3,35 @@ package org.xeneo.core.activity;
 import java.util.Collection;
 import java.util.Map;
 
+/*
+ * The interface aims on the management of Activities.
+ * 
+ *
+ * @author Stefan Huber
+ */
 public interface ActivityManager {
 
-	public void addActivity(Activity activity);
-	public void addActivity(Activity activity, String caseURI, Collection<String> taskURIs);
-	public void addActivity(Activity activity, Map<String,Collection<String>> caseTaskURIs);
-        
-        public boolean isExistingActivity(String activityURI);
-	
+    public void addActivity(Activity activity);
+
+    public void addActivity(Activity activity, String caseURI, Collection<String> taskURIs);
+
+    public void addActivity(Activity activity, Map<String, Collection<String>> caseTaskURIs);
+
+    public boolean isExistingActivity(String activityURI);
+
     /*
      * Add a task context to the Activity for a specific case.
-     * 
-     * @param activityURI The URI of an activity
-     * @param caseURI The URI of a case
-     * @param taskURIs a Collection of task URIs
+     *
+     * @param activityURI The URI of an activity @param caseURI The URI of a
+     * case @param taskURIs a Collection of task URIs
      */
     public void addTaskContexts(String activityURI, String caseURI, Collection<String> taskURIs);
-    public void addTaskContexts(String activityURI, Map<String,Collection<String>> caseTaskURIs);
+
+    public void addTaskContexts(String activityURI, Map<String, Collection<String>> caseTaskURIs);
+
     public void removeTaskContexts(String activityURI, String caseURI, Collection<String> taskURIs);
-    public void removeTaskContexts(String activityURI, Map<String,Collection<String>> caseTaskURIs);
-    
-    public Map<String,Collection<String>> getTaskContexts(String activityURI); 
-	
-	
+
+    public void removeTaskContexts(String activityURI, Map<String, Collection<String>> caseTaskURIs);
+
+    public Map<String, Collection<String>> getTaskContexts(String activityURI);
 }
--- src/main/java/org/xeneo/core/activity/ActivityStream.java
+++ src/main/java/org/xeneo/core/activity/ActivityStream.java
@@ -3,9 +3,10 @@ package org.xeneo.core.activity;
 import java.util.List;
 
 /*
+ * Read-only interface on Activities.
+ * 
  * @author Stefan Huber
  */
-
 public interface ActivityStream {
 
 	public List<Activity> getActivities(String caseURI, String taskURI, int limit);
